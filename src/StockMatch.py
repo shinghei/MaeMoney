c7ad4e0ada616b9305d4af159e9a377aa41ae42f
S. H. Cheung <shing.n900@gmail.com>
2010-06-10 21:52:49 +0800

    Fixed the issue with locating the .qm file

--- src/StockMatch.py
+++ src/StockMatch.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from PyQt4.QtGui import QApplication
 from PyQt4.QtCore import QString, QTranslator
 from StockMatchProperties import StockMatchProperties
+from PyQt4.QtCore import qDebug
 
 if __name__ == '__main__':
     qtApp = QApplication(sys.argv)
     qtApp.setProperty("FingerScrollBars", False)
 
+    # Find out the directory of this file, which has the localization files
+    # (Could have used QCoreApplication.applicationFilePath but it only
+    # returns the path to the python executable on the N900)
+    absFilePath = os.path.abspath(__file__)
+    dir = os.path.dirname(absFilePath)
+
     prop = StockMatchProperties.instance()
     locale = prop.getAppLocale()
     translator = QTranslator()
-    translator.load(QString("app-") + locale.name())
+    loaded = translator.load(QString("app-") + locale.name(), dir)
+    if not loaded:
+        qDebug("Cannot load %s locale from %s" %(locale.name(), dir))
     qtApp.installTranslator(translator)
 
     from StockMatchWin import SMMainWindow
