2070742f0ba5bde1a0a0a732cd3f9dbe335936a6
S. H. Cheung <shing.n900@gmail.com>
2010-06-14 20:57:10 +0800

    Added support for localization and properties management

--- src/MaeMoney.py
+++ src/MaeMoney.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+import sys, os
+
 from MMMainWindow import MMMainWindow
 from MMController import MMController
 
 from PyQt4.QtGui import QApplication
-import sys
+from PyQt4.QtCore import QString, qDebug, QTranslator
+from MaeMoneyProperties import MaeMoneyProperties
 
 class MaeMoney:
     def __init__(self):
@@ -16,5 +19,20 @@ class MaeMoney:
 
 qtApp = QApplication(sys.argv)
 qtApp.setProperty("FingerScrollBars", False)
+
+# Find out the directory of this file, which has the localization files
+# (Could have used QCoreApplication.applicationFilePath but it only
+# returns the path to the python executable on the N900)
+absFilePath = os.path.abspath(__file__)
+dir = os.path.dirname(absFilePath)
+
+prop = MaeMoneyProperties.instance()
+locale = prop.getAppLocale()
+translator = QTranslator()
+loaded = translator.load(QString("app-") + locale.name(), dir)
+if not loaded:
+    qDebug("Cannot load %s locale from %s" %(locale.name(), dir))
+qtApp.installTranslator(translator)
+
 app = MaeMoney()
 sys.exit(qtApp.exec_())
