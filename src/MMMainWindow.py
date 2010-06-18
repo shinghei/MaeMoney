dfde68559c6cc01de7e8a5f6a16861ec903bf0a9
S. H. Cheung <shing.n900@gmail.com>
2010-06-18 18:07:05 +0800

    Added portrait/landscape mode switching. updater interval dialog box and double tap to open Google Finance page

--- src/MMMainWindow.py
+++ src/MMMainWindow.py
@@ -1,34 +1,46 @@
 from PyQt4 import  QtGui
 from MMGoogleClientLoginDialog import MMGoogleClientLoginDialog
 from PortfolioListView import *
-from PyQt4.QtGui import QMainWindow, QLabel, QWidget, QListView, QMenuBar, QComboBox
-from PyQt4.QtCore import SIGNAL, qDebug, QObject, Qt
+from PyQt4.QtGui import QMainWindow, QWidget, QListView, QMenuBar, QComboBox, QGridLayout
+from PyQt4.QtCore import qDebug
+from MaeMoneyProperties import MaeMoneyProperties
+from Util import Util
 
 class MMMainWindow(QMainWindow):
 
     PROP_FINGER_SCROLLABLE = "FingerScrollable"
 
+    '''
+    Redefined some of the attributes here such that Windows doesn't complain
+    http://doc.qt.nokia.com/qt-maemo-4.6/qt.html#WidgetAttribute-enum
+    '''
+    
+    WA_Maemo5LandscapeOrientation = 129
+    WA_Maemo5PortraitOrientation = 128
+    WA_Maemo5AutoOrientation = 130
+    WA_Maemo5ShowProgressIndicator = 131
+
     def __init__(self, controller):
         QMainWindow.__init__(self)
+        self.prop = MaeMoneyProperties.instance()
         self.controller = controller
         self.loginDialog = MMGoogleClientLoginDialog(self)
+        self.switchToLandscapeText = self.tr("Switch to Landscape")
+        self.switchToPortraitText = self.tr("Switch to Portrait")
         self.setupUi()
 
     def setupUi(self):
-        self.setupUiPortrait()
+
+        if self.prop.isPortraitMode():
+            self.setAttributeAndCatch(self.WA_Maemo5PortraitOrientation, True)
+            self.changeOrientationText = self.switchToLandscapeText
+        else:
+            self.setAttributeAndCatch(self.WA_Maemo5LandscapeOrientation, True)
+            self.changeOrientationText = self.switchToPortraitText
+
         self.setWindowTitle(self.tr("MaeMoney"))
-
-    def setupUiPortrait(self):
-
-        self.isPortrait = True
-
-        try:
-            self.setAttribute(Qt.WA_Maemo5PortraitOrientation, True)
-        except AttributeError:
-            qDebug("Cannot set orientation to portrait")
-        self.setMaximumSize(QtCore.QSize(480, 800))
-
-        self.gridLayout = QtGui.QGridLayout()
+        self.setMinimumSize(QtCore.QSize(400, 400))
+        self.gridLayout = QGridLayout()
         widget = QWidget(self)
         widget.setLayout(self.gridLayout)
         self.setCentralWidget(widget)
@@ -36,67 +48,50 @@ class MMMainWindow(QMainWindow):
         self.btnLoadPortfolio = QtGui.QPushButton(self.tr("Sign in to Google Finance"))
         self.gridLayout.addWidget(self.btnLoadPortfolio, 0, 0, 1, 1)
 
+        # List of portfolios
         self.portfolioListView = QComboBox()
-        self.gridLayout.addWidget(self.portfolioListView, 1, 0, 1, 1)
-
-        self.positionsListView = QListView()
-        self.positionsListView.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.positionsListView.setBatchSize(10)
-        self.gridLayout.addWidget(self.positionsListView, 2, 0, 1, 1)
-
-        self.positionsListView.setProperty(self.PROP_FINGER_SCROLLABLE, True)
+        self.portfolioListView.setStyleSheet("QComboBox, QListView { font: 28px; } ")
         self.portfolioListView.setProperty(self.PROP_FINGER_SCROLLABLE, True)
 
-        self.changeAppLocaleAction = QAction(self.tr("Change language"), self)
-        self.changeUrlAction = QAction(self.tr("Change Google Finance URL"), self)
-
-        menuBar = QMenuBar()
-        menuBar.addAction(self.changeAppLocaleAction)
-        menuBar.addAction(self.changeUrlAction)
-        self.setMenuBar(menuBar)
-
-    def setupUiLandscape(self):
-
-        self.isPortrait = True        
-
-        self.setMaximumSize(QtCore.QSize(800, 480))
-
-        self.gridLayout = QtGui.QGridLayout()
-        widget = QWidget(self)
-        widget.setLayout(self.gridLayout)
-        self.setCentralWidget(widget)
-
-        self.btnLoadPortfolio = QtGui.QPushButton(self.tr("Sign in to Google Finance"))
-        self.gridLayout.addWidget(self.btnLoadPortfolio, 0, 0, 1, 2)
-
-        self.portfolioListView = PortfolioListView(self)
-        self.gridLayout.addWidget(self.portfolioListView, 1, 0, 1, 1)
-
+        # Positions within the selected portfolio
         self.positionsListView = QListView()
-        self.positionsListView.setMinimumWidth(600)
         self.positionsListView.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.positionsListView.setBatchSize(10)
-#        self.positionsListView.setLayoutMode(QListView.Batched)
-#        self.setFlow(QListView.LeftToRight)
-#        self.setHorizontalScrollMode(QAbstractItemView.ScrollPerItem)
-        self.gridLayout.addWidget(self.positionsListView, 1, 1, 1, 1)
-
         self.positionsListView.setProperty(self.PROP_FINGER_SCROLLABLE, True)
-        self.portfolioListView.setProperty(self.PROP_FINGER_SCROLLABLE, True)
-
-        self.changeAppLocaleAction = QAction(self.tr("Change language"), self)
-        self.changeUrlAction = QAction(self.tr("Change Google Finance URL"), self)
 
         menuBar = QMenuBar()
-        menuBar.addAction(self.changeAppLocaleAction)
-        menuBar.addAction(self.changeUrlAction)
         self.setMenuBar(menuBar)
+
+        self.changeAppLocaleAction = QAction(self.tr("Change language"), self)
+        menuBar.addAction(self.changeAppLocaleAction)
+
+        self.changeUrlAction = QAction(self.tr("Change Google Finance URL"), self)
+        menuBar.addAction(self.changeUrlAction)
+
+        self.changeOrientationAction = QAction(self.changeOrientationText, self)
+        menuBar.addAction(self.changeOrientationAction)
+
+        self.changeUpdateIntervalAction = QAction(self.tr("Change update interval"), self)
+        menuBar.addAction(self.changeUpdateIntervalAction)
+
+    def changeOrientation(self):
+        if self.prop.isPortraitMode():
+            self.setAttributeAndCatch(self.WA_Maemo5LandscapeOrientation, True)
+            self.prop.setPortraitMode(False)
+            self.resize(800, 480)
+            self.changeOrientationAction.setText(self.switchToPortraitText)
+        else:
+            self.setAttributeAndCatch(self.WA_Maemo5PortraitOrientation, True)
+            self.prop.setPortraitMode(True)
+            self.resize(480, 800)
+            self.changeOrientationAction.setText(self.switchToLandscapeText)
 
     def removeLoginButton(self):
         self.btnLoadPortfolio.setParent(None)
         self.gridLayout.removeWidget(self.btnLoadPortfolio)
 
     def setPortfolioListModel(self, model):
+        self.gridLayout.addWidget(self.portfolioListView, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.positionsListView, 1, 1, 1, 1)
         self.portfolioListView.setModel(model)
 
     def setPositionsModel(self, model):
@@ -114,21 +109,30 @@ class MMMainWindow(QMainWindow):
         self.positionsViewDelegate = PositionsViewDelegate()
         self.positionsListView.setItemDelegate(self.positionsViewDelegate)
 
-    def setupSelectionModel(self):
-        selectionModel = self.positionsListView.selectionModel()
-        self.connect(selectionModel,
-                     SIGNAL("selectionChanged(QItemSelection)"),
-                     self.entrySelected)
-
-    def entrySelected(self, selected):
-         pass
-#        self.selectedPositionIndex = selected
-#        tickerSelected = self.positionsModel.getTicker(self.selectedPositionIndex)
 
     def setBusyStatus(self, busy):
         try:
-            self.setAttribute(Qt.WA_Maemo5ShowProgressIndicator, busy)
+            self.setAttribute(self.WA_Maemo5ShowProgressIndicator, busy)
             from PyQt4.QtGui import QApplication
             QApplication.processEvents()
         except AttributeError:
             qDebug("Can't use WA_Maemo5ShowProgressIndicator")
+
+    def setAttributeAndCatch(self, attribute, trueFalse):
+        try:
+            self.setAttribute(attribute, trueFalse)
+        except AttributeError:
+            qDebug("Can't set attribute %d" %(attribute))
+
+    def positionDoubleClicked(self, modelIndex):
+        '''
+        @param QModelIndex modelIndex
+        '''
+
+        exchange = self.positionsModel.getExchange(modelIndex)
+        ticker = self.positionsModel.getTicker(modelIndex)
+        exchangeTicker = "%s:%s" % (exchange, ticker)
+        googleUrl = self.prop.getGoogleUrl()
+        url = "http://%s/finance?q=%s" % (googleUrl, exchangeTicker)
+        qDebug("Opening URL: %s" %(url))
+        Util.openUrl(url)
