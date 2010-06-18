4dabd309c482b92e7b1d825e5ad6d50b66970a0f
S. H. Cheung <shing.n900@gmail.com>
2010-06-18 18:27:11 +0800

    Prepare for v0.2 release

--- src/app-zh_HK.ts
+++ src/app-zh_HK.ts
@@ -43,9 +43,42 @@
     </message>
 </context>
 <context>
+    <name>LandscapeSpecificDelegate</name>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="170"/>
+        <source>P/E ratio: </source>
+        <translation>市盈率: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="174"/>
+        <source>Market Cap.: </source>
+        <translation>總市值: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="180"/>
+        <source>Daily / Avg Vol.: </source>
+        <translation>成交量/平均值: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="186"/>
+        <source>Realtime data</source>
+        <translation>即時資料</translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="188"/>
+        <source>Delay: </source>
+        <translation>資料延遲: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="188"/>
+        <source> minutes</source>
+        <translation> 分鐘</translation>
+    </message>
+</context>
+<context>
     <name>MMController</name>
     <message>
-        <location filename="MMController.py" line="146"/>
+        <location filename="MMController.py" line="156"/>
         <source>Loading portfolios from Google</source>
         <translation>正在擷取投資組合</translation>
     </message>
@@ -81,24 +114,77 @@
         <translation type="obsolete">&lt;-- 按此登入Google財經網</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="88"/>
+        <location filename="MMMainWindow.py" line="67"/>
         <source>Change Google Finance URL</source>
         <translation>更改Google財經網址</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="19"/>
+        <location filename="MMMainWindow.py" line="41"/>
         <source>MaeMoney</source>
         <translation>MaeMoney</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="69"/>
+        <location filename="MMMainWindow.py" line="48"/>
         <source>Sign in to Google Finance</source>
         <translation>登入Google財經網</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="87"/>
+        <location filename="MMMainWindow.py" line="64"/>
         <source>Change language</source>
         <translation>更改語言</translation>
+    </message>
+    <message>
+        <location filename="MMMainWindow.py" line="28"/>
+        <source>Switch to Landscape</source>
+        <translation>轉為橫向</translation>
+    </message>
+    <message>
+        <location filename="MMMainWindow.py" line="29"/>
+        <source>Switch to Portrait</source>
+        <translation>轉為直向</translation>
+    </message>
+    <message>
+        <location filename="MMMainWindow.py" line="73"/>
+        <source>Change update interval</source>
+        <translation>設置更新週期</translation>
+    </message>
+</context>
+<context>
+    <name>PortraitSpecificDelegate</name>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="91"/>
+        <source>PE ratio: </source>
+        <translation>市盈率: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="95"/>
+        <source>Mkt Cap: </source>
+        <translation>總市值: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="100"/>
+        <source>Vol: </source>
+        <translation>成交量: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="105"/>
+        <source>Avg Vol: </source>
+        <translation>平均成交量: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="111"/>
+        <source>Realtime data</source>
+        <translation>即時資料</translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="113"/>
+        <source>Delay: </source>
+        <translation>延遲: </translation>
+    </message>
+    <message>
+        <location filename="PositionsViewDelegate.py" line="113"/>
+        <source> mins</source>
+        <translation> 分鐘</translation>
     </message>
 </context>
 <context>
@@ -135,6 +221,84 @@
         <location filename="StockMatchTableModel.py" line="129"/>
         <source>Name</source>
         <translation>名稱</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateIntervalDialog</name>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="48"/>
+        <source>Update Interval</source>
+        <translation>更新週期</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="29"/>
+        <source>Pause update</source>
+        <translation>暫停更新</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="30"/>
+        <source>Update interval - %ds</source>
+        <translation type="obsolete">更新週期 - %d秒</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="33"/>
+        <source>Update interval - 25s</source>
+        <translation>更新週期 - 25秒</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="34"/>
+        <source>Update interval - 1 min</source>
+        <translation>更新週期 - 1分鐘</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="35"/>
+        <source>Update interval - 5 min</source>
+        <translation>更新週期 - 5分鐘</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="36"/>
+        <source>Update interval - 15 min</source>
+        <translation>更新週期 - 15分鐘</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="37"/>
+        <source>Update interval - 30 min</source>
+        <translation>更新週期 - 30分鐘</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="38"/>
+        <source>Update interval - 45 min</source>
+        <translation>更新週期 - 45分鐘</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="39"/>
+        <source>Update interval - 1 hr</source>
+        <translation>更新週期 - 1小時</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="40"/>
+        <source>Update interval - 2 hr</source>
+        <translation>更新週期 - 2小時</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="45"/>
+        <source>Back to Main Application</source>
+        <translation>返回</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="89"/>
+        <source>Timer currently active. Click to pause update</source>
+        <translation>更新進行中，按此暫停</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="92"/>
+        <source>Update not running</source>
+        <translation>更新已暫停</translation>
+    </message>
+    <message>
+        <location filename="UpdateIntervalDialog.py" line="30"/>
+        <source>Update interval - %1s</source>
+        <translation>更新週期 - %1秒</translation>
     </message>
 </context>
 </TS>
