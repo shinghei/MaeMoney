d6944fbd1303bfff9b290553b4d8e5419df30d19
S. H. Cheung <shing.n900@gmail.com>
2010-07-25 18:43:26 +0800

    localization

--- src/TransactionsModelPortrait.py
+++ src/TransactionsModelPortrait.py
@@ -1,9 +1,7 @@
-from PyQt4.QtCore import QAbstractTableModel, QModelIndex, Qt, QVariant, QString
-from Util import Util
+from PyQt4.QtCore import QAbstractTableModel, QModelIndex, Qt, QVariant, QString, QObject
+from TransactionDataWrapper import TransactionDataWrapper
 
-class TransactionsModel(QAbstractTableModel):
-
-    HEADERS = ["Type", "Date", "Shares @\nPrice", "Value +\nCommission"]
+class TransactionsModelPortrait(QAbstractTableModel):
     COL_TYPE = 0
     COL_DATE = 1
     COL_SHARES_PRICE = 2
@@ -12,47 +10,53 @@ class TransactionsModel(QAbstractTableModel):
     '''
     @param transactions - list of TransactionData
     '''
+
     def __init__(self, transactions):
         QAbstractTableModel.__init__(self)
         self.transactions = transactions
+        self.HEADERS = [self.tr("Type"),
+                        self.tr("Date"),
+                        self.tr("Shares @\nPrice"),
+                        self.tr("Value +\nCommission")]
 
-    def rowCount(self, parent = QModelIndex()):
+
+    def rowCount(self, parent=QModelIndex()):
         return len(self.transactions)
 
-    def columnCount(self, parent = QModelIndex()):
+    def columnCount(self, parent=QModelIndex()):
         return len(self.HEADERS)
 
-    def data(self,  index, role = Qt.DisplayRole ):
-
+    def data(self, index, role=Qt.DisplayRole ):
         row = index.row()
         col = index.column()
 
         if role == Qt.DisplayRole:
-
             transactData = self.transactions[row].transaction_data
+            transactDataWrapper = TransactionDataWrapper(transactData)
 
             if col == self.COL_TYPE:
-                return transactData.type
+                return transactDataWrapper.getType()
 
             elif col == self.COL_DATE:
-                date = Util.extractDate(transactData.date)
+                date = transactDataWrapper.getDate()
                 if date is None:
                     return "N/A"
                 return date
 
             elif col == self.COL_SHARES_PRICE:
-                return QString("%s @\n%s" %(transactData.shares, transactData.price.money[0].amount))
+                shares = transactDataWrapper.getShares()
+                unitPrice = transactDataWrapper.getUnitPrice()
+                return QString("%.0f @\n%.2f" % (shares, unitPrice))
 
             elif col == self.COL_CASH_VALUE_COMMISSION:
-                share = self.strToFloat(transactData.shares)
-                price = self.strToFloat(transactData.price.money[0].amount)
-                commission = self.strToFloat(transactData.commission.money[0].amount)
-                currency = transactData.commission.money[0].currency_code
-                cashValue = share * price + commission
+                shares = transactDataWrapper.getShares()
+                unitPrice = transactDataWrapper.getUnitPrice()
+                commission = transactDataWrapper.getCommission()
+                currency = transactDataWrapper.getCurrency()
+                cashValue = shares * unitPrice + commission
                 return "%.2f\n+ %s%.2f" % (cashValue, currency, commission)
 
         elif role == Qt.TextAlignmentRole:
-
             if col == self.COL_TYPE:
                 return Qt.AlignLeft | Qt.AlignVCenter
 
@@ -67,12 +71,6 @@ class TransactionsModel(QAbstractTableModel):
 
         return QVariant()
 
-    def headerData(self, section, orientation, role = Qt.DisplayRole):
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
         if role == Qt.DisplayRole and orientation == Qt.Horizontal:
             return self.HEADERS[section]
-
-    def strToFloat(self, s):
-        try:
-            return float(s)
-        except ValueError:
-            return None
