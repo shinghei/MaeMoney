ce3624d43c7b8aa0dc1dd2c62c6497d752ef0a7e
S. H. Cheung <shing.n900@gmail.com>
2010-07-25 16:19:37 +0800

    continued development

--- src/PortfolioListView.py
+++ src/PortfolioListView.py
@@ -1,12 +1,18 @@
-from PyQt4.QtGui import QListView, QAbstractItemView, QMenu,  QAction
+from PyQt4.QtGui import QListView, QAbstractItemView, QMenu, QAction
 from PyQt4 import QtCore
-from PyQt4.QtCore import QEvent
+from PyQt4.QtCore import QEvent, SIGNAL, QObject, QPoint
+from MaeMoneyProperties import MaeMoneyProperties
 
-class PortfolioListView(QListView):
+class PortfolioListView(QListView, QObject):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent):
+        '''
+        @param parent - QMainWindow
+        '''
+
         QListView.__init__(self, parent)
 
+        self.parentWindow = parent
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setSelectionBehavior(QAbstractItemView.SelectItems)
         self.viewport().installEventFilter(self)
@@ -16,15 +22,56 @@ class PortfolioListView(QListView):
         @param obj QObject
         @paramn event QEvent
         '''
-       
-#        if (event.type() == QEvent.MouseButtonPress):
-#            print "Mouse pressed"
-#
+
+        #        if (event.type() == QEvent.MouseButtonPress):
+        #            print "Mouse pressed"
+        #
         if (event.type() == QEvent.ContextMenu):
-            print "Right clicked!"
+
+            # Now we know that event is an instance of QContextMenuEvent
+
             menu = QMenu(self)
 
-            menu.addAction(QAction("New",self))
+            newPos = self.adjustPosition(event.pos())
+            modelIndex = self.indexAt(newPos)
+
+            self.showTransactions = QAction(self.tr("Show transactions"), self)
+            menu.addAction(self.showTransactions)
+            self.connect(self.showTransactions,
+                         SIGNAL("triggered()"),
+                         lambda : self.parentWindow.showTransactionsForPosition(modelIndex))
+
+            self.openWebpage = QAction(self.tr("Open Google Finance webpage"), self)
+            menu.addAction(self.openWebpage)
+            self.connect(self.openWebpage,
+                         SIGNAL("triggered()"),
+                         lambda : self.parentWindow.openWebpageForPosition(modelIndex))
+
             menu.exec_(event.globalPos())
 
+
         return QListView.eventFilter(self, obj, event)
+
+
+    '''
+    Due to a bug in PyQt on Maemo (or probably because I haven't yet figured out), the right clicked position
+    is off.
+    '''
+    def adjustPosition(self, eventPos):
+        '''
+        @param eventPos - QPoint
+        '''
+        prop = MaeMoneyProperties.instance()
+
+        newPos = QPoint(eventPos.x(), eventPos.y())
+        if prop.isPortraitMode():
+            offset = 50
+        else:
+            offset = 72
+
+        newY = max(0, eventPos.y() - offset)
+        newPos.setY(newY)
+
+
+        return newPos
+
