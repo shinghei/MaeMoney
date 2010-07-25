47889a3be73394b37640377bd2159456959b9fc9
S. H. Cheung <shing.n900@gmail.com>
2010-07-25 18:44:06 +0800

    localization

--- src/app-zh_HK.ts
+++ src/app-zh_HK.ts
@@ -4,17 +4,17 @@
 <context>
     <name>AppLocaleSetupDialog</name>
     <message>
-        <location filename="AppLocaleSetupDialog.py" line="39"/>
+        <location filename="AppLocaleSetupDialog.py" line="40"/>
         <source>Current setting</source>
         <translation>現有設定</translation>
     </message>
     <message>
-        <location filename="AppLocaleSetupDialog.py" line="43"/>
+        <location filename="AppLocaleSetupDialog.py" line="44"/>
         <source>Set language</source>
         <translation>設置語言</translation>
     </message>
     <message>
-        <location filename="AppLocaleSetupDialog.py" line="46"/>
+        <location filename="AppLocaleSetupDialog.py" line="47"/>
         <source>Language setup</source>
         <translation>語言選擇</translation>
     </message>
@@ -78,7 +78,7 @@
 <context>
     <name>MMController</name>
     <message>
-        <location filename="MMController.py" line="156"/>
+        <location filename="MMController.py" line="175"/>
         <source>Loading portfolios from Google</source>
         <translation>正在擷取投資組合</translation>
     </message>
@@ -86,19 +86,24 @@
 <context>
     <name>MMGoogleClientLoginDialog</name>
     <message>
-        <location filename="MMGoogleClientLoginDialog.py" line="39"/>
+        <location filename="MMGoogleClientLoginDialog.py" line="41"/>
         <source>Password</source>
         <translation>密碼</translation>
     </message>
     <message>
-        <location filename="MMGoogleClientLoginDialog.py" line="34"/>
+        <location filename="MMGoogleClientLoginDialog.py" line="36"/>
         <source>User name</source>
         <translation>帳戶</translation>
     </message>
     <message>
-        <location filename="MMGoogleClientLoginDialog.py" line="45"/>
+        <location filename="MMGoogleClientLoginDialog.py" line="47"/>
         <source>Login</source>
         <translation>登入</translation>
+    </message>
+    <message>
+        <location filename="MMGoogleClientLoginDialog.py" line="29"/>
+        <source>Login to Google Finance</source>
+        <translation>登入Google財經網</translation>
     </message>
 </context>
 <context>
@@ -114,39 +119,52 @@
         <translation type="obsolete">&lt;-- 按此登入Google財經網</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="67"/>
+        <location filename="MMMainWindow.py" line="69"/>
         <source>Change Google Finance URL</source>
         <translation>更改Google財經網址</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="41"/>
+        <location filename="MMMainWindow.py" line="43"/>
         <source>MaeMoney</source>
         <translation>MaeMoney</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="48"/>
+        <location filename="MMMainWindow.py" line="50"/>
         <source>Sign in to Google Finance</source>
         <translation>登入Google財經網</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="64"/>
+        <location filename="MMMainWindow.py" line="66"/>
         <source>Change language</source>
         <translation>更改語言</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="28"/>
+        <location filename="MMMainWindow.py" line="30"/>
         <source>Switch to Landscape</source>
         <translation>轉為橫向</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="29"/>
+        <location filename="MMMainWindow.py" line="31"/>
         <source>Switch to Portrait</source>
         <translation>轉為直向</translation>
     </message>
     <message>
-        <location filename="MMMainWindow.py" line="73"/>
+        <location filename="MMMainWindow.py" line="75"/>
         <source>Change update interval</source>
         <translation>設置更新週期</translation>
+    </message>
+</context>
+<context>
+    <name>PortfolioListView</name>
+    <message>
+        <location filename="PortfolioListView.py" line="38"/>
+        <source>Show transactions</source>
+        <translation>成交記錄</translation>
+    </message>
+    <message>
+        <location filename="PortfolioListView.py" line="44"/>
+        <source>Open Google Finance webpage</source>
+        <translation>開啟Google財經網</translation>
     </message>
 </context>
 <context>
@@ -188,6 +206,53 @@
     </message>
 </context>
 <context>
+    <name>QObject</name>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="6"/>
+        <source>Type</source>
+        <translation type="obsolete">類型</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="7"/>
+        <source>Date</source>
+        <translation type="obsolete">日期</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="7"/>
+        <source>Shares</source>
+        <translation type="obsolete">股份</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="8"/>
+        <source>Price</source>
+        <translation type="obsolete">價格</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="9"/>
+        <source>Value</source>
+        <translation type="obsolete">現金值</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="10"/>
+        <source>Commission</source>
+        <translation type="obsolete">佣金</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="8"/>
+        <source>Shares @
+Price</source>
+        <translation type="obsolete">股份 @
+價格</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="9"/>
+        <source>Value +
+Commission</source>
+        <translation type="obsolete">現金值 +
+佣金</translation>
+    </message>
+</context>
+<context>
     <name>SMMainWindow</name>
     <message>
         <location filename="StockMatchWin.py" line="26"/>
@@ -221,6 +286,66 @@
         <location filename="StockMatchTableModel.py" line="129"/>
         <source>Name</source>
         <translation>名稱</translation>
+    </message>
+</context>
+<context>
+    <name>TransactionsModelLandscape</name>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="19"/>
+        <source>Type</source>
+        <translation>類型</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="20"/>
+        <source>Date</source>
+        <translation>日期</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="21"/>
+        <source>Shares</source>
+        <translation>股份</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="22"/>
+        <source>Price</source>
+        <translation>價格</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="23"/>
+        <source>Value</source>
+        <translation>現金值</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelLandscape.py" line="24"/>
+        <source>Commission</source>
+        <translation>佣金</translation>
+    </message>
+</context>
+<context>
+    <name>TransactionsModelPortrait</name>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="17"/>
+        <source>Type</source>
+        <translation>類型</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="18"/>
+        <source>Date</source>
+        <translation>日期</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="19"/>
+        <source>Shares @
+Price</source>
+        <translation>股份 @
+價格</translation>
+    </message>
+    <message>
+        <location filename="TransactionsModelPortrait.py" line="20"/>
+        <source>Value +
+Commission</source>
+        <translation>現金值 +
+佣金</translation>
     </message>
 </context>
 <context>
